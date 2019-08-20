7458aa22a26b7095aced1107633f4c0d29a97cfa
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-20 17:50:17 +0100

    progress bar sample simulator

--- graphics2.java
+++ graphics2.java
@@ -7,10 +7,10 @@ public class graphics2 extends Applet {
 	Timer t ;
 	TimerTask tk =new TimerTask(){
 			public void run(){
+				Graphics g =getGraphics();
 				if (ii < 100){
-					ii=ii+1;
-					repaint();
-					//t.schedule(tk,100);
+					paint(g);
+			
 				}
 				if (ii > 100) t.cancel();
 				
@@ -20,7 +20,7 @@ public class graphics2 extends Applet {
 
 	
 	public void init(){
-		ii=1;
+		setBackground(new Color(230,230,230));
 		t = new Timer("Timer");
 		t.scheduleAtFixedRate(tk,100,100);
 	}
@@ -32,11 +32,13 @@ public class graphics2 extends Applet {
 	public void paint (Graphics g) {
 		int i;
 		String s;
-		for(i=0;i<ii;i++){
-			g.setColor(new Color(255,i*2,i*2));
-			g.drawLine(i+20,20,i+20,40);
-
-		}
+		s=Integer.toString(ii)+" % ";
+		g.setColor(new Color(230,230,230));
+		g.drawString(s,70,30);		
+		ii=ii+1;
+		i=ii;
+		g.setColor(new Color(255,i*2,i*2));
+		g.drawLine(i+20,20,i+20,40);
 		s=Integer.toString(ii)+" % ";
 		g.setColor(new Color(0,0,0));
 		g.drawString(s,70,30);		
