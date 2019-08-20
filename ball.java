1e29e1d10840c4f364961205db365da292094650
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-20 17:23:00 +0100

    jamping ball animation

--- ball.java
+++ ball.java
@@ -9,17 +9,22 @@ public class ball extends Applet {
 	int xx=10;
 	int yy=10;
 	Color c=new Color(255,0,0);
+	Color cc=new Color(230,230,230);
 	Timer t ;
 	TimerTask tk =new TimerTask(){
 			public void run(){
+				Graphics g =getGraphics();
 				if (ii < 100){
+					g.setColor(cc);
+					g.fillOval(x,y,20,20);
 					x=x+xx;
 					y=y+yy;
 					if (x>580) xx=-(xx);
 					if (y>280) yy=-(yy);
 					if (x<4) xx=-(xx);
 					if (y<4) yy=-(yy);
-					repaint();
+					paint(g);
+
 				}
 				if (ii > 100) t.cancel();
 				
@@ -30,6 +35,7 @@ public class ball extends Applet {
 	
 	public void init(){
 		ii=1;
+		setBackground(cc);
 		t = new Timer("Timer");
 		t.scheduleAtFixedRate(tk,100,100);
 	}
